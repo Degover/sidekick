b92ab65fc308342e3ae3c0a9d905249b3e6a161f
Degover <chaors2000@gmail.com>
2023-02-24 21:26:47 -0300

    The app should not steal focus

--- View/Impl/MainForm.cs
+++ View/Impl/MainForm.cs
@@ -12,5 +12,18 @@ namespace Sidekick.View.Impl
 
             _presenter = new MainPresenter(this);
         }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams baseParams = base.CreateParams;
+
+                const int WS_EX_NOACTIVATE = 0x08000000;
+                baseParams.ExStyle = WS_EX_NOACTIVATE;
+
+                return baseParams;
+            }
+        }
     }
 }
